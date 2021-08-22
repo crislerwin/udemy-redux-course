ba575e272805f5506bca256a47f1374656ca70b1
crislerwin <crisler.silva@alunos.fael.edu.br>
2021-08-22 11:06:22 -0300

    ✅ create  action busca-video action

---
+++ youtube/src/redux/actions/busca-video.js
@@ -0,0 +1,37 @@
+import youtubeSearch from 'youtube-api-v3-search';
+
+const API_KEY = process.env.YOUTUBE_API_KEY;
+
+export const buscaVideoRequest = () => {
+    return {
+        type: 'BUSCA_VIDEO_REQUEST',
+        carregando: true,
+        erro: false
+    }
+}
+
+export const buscaVideoSucess = (videos) => {
+    return {
+        type: 'BUSCA_VIDEO_SUCCESS',
+        videos,
+        carregando: false,
+        erro: false
+    }
+}
+
+export const buscaVideoError = () => {
+    return {
+        type: 'BUSCA_VIDEO_ERROR',
+        carregando: false,
+        erro: true
+    }
+}
+
+export const buscaVideo = (termo) => {
+    return dispatch => {
+        dispatch(buscaVideoRequest())
+        youtubeSearch(API_KEY, { q: termo })
+            .then((data) => dispatch(buscaVideoSucess(data.items)))
+            .catch(() => dispatch(buscaVideoError()))
+    }
+}
