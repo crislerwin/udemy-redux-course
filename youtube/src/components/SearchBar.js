5905a7a078c7a47e100df3cd19799586387eaca4
crislerwin <crisler.silva@alunos.fael.edu.br>
2021-08-22 16:05:19 -0300

    ✨ connect search-bar with redux

--- youtube/src/components/SearchBar.js
+++ youtube/src/components/SearchBar.js
@@ -1,36 +1,34 @@
-import React, { Component } from 'react'
-import { Segment, Input}  from 'semantic-ui-react'
-import {buscaVideo} from '../redux/actions/busca-video'
-import {connect } from 'react-redux'
+import { Component } from "react";
+import { Segment, Input } from "semantic-ui-react";
+import { buscaVideo } from "../redux/actions/busca-video";
+import { connect } from "react-redux";
 class SearchBar extends Component {
-
-
-    buscaVideo = (e) => {
-        if (e.keyCode === 13){
-            const termo = e.target.value
-            console.log(termo)
-            this.props.buscaVideo(termo)
-            
-        }
-
+  buscaVideo = (e) => {
+    if (e.keyCode === 13) {
+      const termo = e.target.value;
+      console.log(termo);
+      this.props.buscaVideo(termo);
     }
+  };
 
-    render() {
-        return (
-
-         <div className="search-bar">
-            <Segment stacked>
-                <Input icon='search' placeholder='Search...' onKeyDown={this.buscaVideo} />
-            </Segment>
-         </div>
-        )
-    }
+  render() {
+    return (
+      <div className="search-bar">
+        <Segment stacked>
+          <Input
+            icon="search"
+            placeholder="Search..."
+            onKeyDown={this.buscaVideo}
+          />
+        </Segment>
+      </div>
+    );
+  }
 }
 
-const mapDispatchToProps = dispatch => {
-    return {
-        buscaVideo: (termo) => dispatch(buscaVideo(termo))
-    }
-
-}
-export default connect(null, mapDispatchToProps)(SearchBar)
+const mapDispatchToProps = (dispatch) => {
+  return {
+    buscaVideo: (termo) => dispatch(buscaVideo(termo)),
+  };
+};
+export default connect(null, mapDispatchToProps)(SearchBar);
